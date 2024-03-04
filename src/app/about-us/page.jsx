1d11e218532ae787c86fff8c07cbf9ab4279a236
AkramAltaf <41393849+AkramAltaf@users.noreply.github.com>
2024-03-05 00:09:17 +0530

    CSE-502: img removed

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,11 +1,12 @@
 import styles from "./about-us.module.css";
 import image1 from "../../../public/images/logo.svg";
+import Image from "next/image";
 
 const AboutPage = () => {
   return (
     <div className={`container ${styles.container}`}>
       <h2 className={styles.header}>Heading</h2>
-      <img src={image1} alt="image" />
+      <Image src={image1} alt="image" />
       Blog page
     </div>
   );
