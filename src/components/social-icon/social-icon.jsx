e53446862cb21fce488786d771a195cfad74ccb8
AkramAltaf <41393849+AkramAltaf@users.noreply.github.com>
2024-03-27 00:01:45 +0530

    CSE-502:social links added

--- src/components/social-icon/social-icon.jsx
+++ src/components/social-icon/social-icon.jsx
@@ -1,21 +1,42 @@
 import Link from "next/link";
 import styles from "./social-icon.module.css";
-import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+} from "react-icons/fa";
 
 const SocialIcon = (props) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.socialIcon}>
-        <Link style={props.style} href="#">
+        <Link
+          style={props.style}
+          target="--blank"
+          href="https://www.facebook.com/projectleadersmagazine"
+        >
           <FaFacebookF />
         </Link>
-        <Link style={props.style} href="#">
-          <FaTwitter />
+        <Link
+          style={props.style}
+          target="--blank"
+          href="https://www.linkedin.com/company/projectleadersmagazine/"
+        >
+          <FaLinkedin />
         </Link>
-        <Link style={props.style} href="#">
+        <Link
+          style={props.style}
+          target="--blank"
+          href="http://www.youtube.com/@ProjectLeadersMagazine-jd8lh"
+        >
           <FaYoutube />
         </Link>
-        <Link style={props.style} href="#">
+        <Link
+          style={props.style}
+          target="--blank"
+          href="https://www.instagram.com/projectleadersmagazine/"
+        >
           <FaInstagram />
         </Link>
       </div>
