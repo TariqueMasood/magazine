6248a61c13c98107fa96f9e38e4e5d17f5f3d1fa
AkramAltaf <41393849+AkramAltaf@users.noreply.github.com>
2024-04-06 01:25:23 +0530

    CSE-502:webinar card done

--- src/app/webinar/page.jsx
+++ src/app/webinar/page.jsx
@@ -1,493 +1,100 @@
-"use client";
-
-import MgButton from "@/components/button/button";
-import heroImg1 from "../../../public/images/wb-img1.png";
-import heroImg2 from "../../../public/images/wb-img2.png";
-import heroImg3 from "../../../public/images/wb-img3.png";
-import ellipseImg from "../../../public/images/wb-ellipse.png";
+import { Container } from "react-bootstrap";
 import styles from "./webinar.module.css";
-import { Col, Container, Row } from "react-bootstrap";
-import Image from "next/image";
 import SectionTitle from "@/components/section-title/section-title";
 import { MdKeyboardArrowRight } from "react-icons/md";
-import cardImg1 from "../../../public/images/textual-interview-img1.png";
-import cardImg2 from "../../../public/images/textual-interview-img2.png";
-import cardImg3 from "../../../public/images/textual-interview-img3.png";
-import cardImg4 from "../../../public/images/textual-interview-img4.png";
-import cardImg5 from "../../../public/images/textual-interview-img5.png";
-import cardImg6 from "../../../public/images/textual-interview-img6.png";
-import YouTube from "react-youtube";
 import Link from "next/link";
+import Image from "next/image";
+import webinarImg1 from "../../../public/images/webinar-img-1.webp";
+import MgButton from "@/components/button/button";
 
-const YouTubePlayer = ({ videoId, width, height }) => {
-  const opts = {
-    width: width || "100%",
-    height: height || "400px",
-  };
+const FeaturedCard = () => {
   return (
-    <div>
-      <YouTube videoId={videoId} opts={opts} />
-    </div>
-  );
-};
-
-const FirstRowInterview = () => {
-  return (
-    <div className={styles.rowInterview}>
-      <div className={styles.videoViewAll}>
-        <SectionTitle title="video interviews" />
-        <Link href="/webinar/video-interviews">
-          View All <MdKeyboardArrowRight />
-        </Link>
+    <Link className={styles.FeaturedCardWrapper} href="#">
+      <div className={styles.featuredCard}>
+        <div className={styles.featuredCardImgContainer}>
+          <Image
+            className={styles.featuredCardImg}
+            src={webinarImg1}
+            alt="webinarImage"
+          />
+        </div>
+        <div className={styles.featuredCardContentWrapper}>
+          <div className={styles.featuredCardContent}>
+            <span className={styles.featuredCardTitle}>featured</span>
+            <h3>tech & AI live london 2024</h3>
+            <div className={styles.featuredCardDate}>
+              Tue 21 May, 2024. 14:00 GMT. Online (virtual)
+            </div>
+            <div className={styles.featuredCardDetail}>
+              connecting tech & AI leaders across europe
+            </div>
+          </div>
+          <MgButton>
+            Register Now <MdKeyboardArrowRight />
+          </MgButton>
+        </div>
       </div>
-      <p>New and archived interviews conducted by our editorial.</p>
-      <Row>
-        <Col lg={8}>
-          <div className={styles.FirstVideo}>
-            <div>
-              <div className={styles.videoContainer}>
-                <YouTubePlayer
-                  videoId="meT67QQqrIw"
-                  width="100%"
-                  height="420px"
-                />
-              </div>
-              <div className={styles.videoContainer}>
-                <h3>Exclusive Video: Red Hat at MWC 2024</h3>
-                <h6>Digital Transformation</h6>
-                <p>
-                  We speak with Ben Panic, Vice President of Red Hat’s Global
-                  Telco Centre of Excellence, about the company’s MWC 2024
-                  announcements and future telco trends
-                </p>
-              </div>
-            </div>
-          </div>
-        </Col>
-        <Col lg={4}>
-          <div className={styles.SecondVideo}>
-            <div className={styles.videoContainer}>
-              <YouTubePlayer
-                videoId="WbwEtxEqUEU"
-                width="100%"
-                height="420px"
-              />
-            </div>
-            <div className={styles.SecondVideoContent}>
-              <h6>Cloud & Cyber Security</h6>
-              <p>
-                Concurrency&apos;s company cloud strategies to help reduce waste
-              </p>
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </div>
+    </Link>
   );
 };
 
-const SecondRowInterview = () => {
+const WebinarCard = () => {
   return (
-    <div>
-      <Row>
-        <Col lg={4}>
-          <div className={styles.secondRowCard}>
-            <div className={styles.videoContainer}>
-              <YouTubePlayer
-                videoId="NZEN-ItKL2k&t=229s"
-                width="100%"
-                height="250px"
-              />
-            </div>
-            <h6>Digital Transformation</h6>
-            <p>Mobile World Congress 2024: Day Two Highlights</p>
+    <Link className={styles.webinarCardWrapper} href="#">
+      <div className={styles.webinardCard}>
+        <div className={styles.webinarCardImg}>
+          <Image src={webinarImg1} alt="pic" />
+        </div>
+        <div className={styles.webinarCardContent}>
+          <div>
+            <h5>australian CIO summit 2024</h5>
+            <h6>mon 22 july, 2024 . 12:30 GMT. queensland, australia</h6>
+            <p>
+              The Australian CIO Summit aims to foster innovative thinking,
+              share inspiring ideas, and promote community connection.
+            </p>
           </div>
-        </Col>
-        <Col lg={4}>
-          <div className={styles.secondRowCard}>
-            <div className={styles.videoContainer}>
-              <YouTubePlayer
-                videoId="AXKjjfxNhJk&t475s"
-                width="100%"
-                height="250px"
-              />
-            </div>
-            <h6>Head of IT & Compliance at GFS</h6>
-            <p>Marching to the beat of digital transformation</p>
+          <div>
+            <MgButton>Register Now</MgButton>
           </div>
-        </Col>
-        <Col lg={4}>
-          <div className={styles.secondRowCard}>
-            <div className={styles.videoContainer}>
-              <YouTubePlayer
-                videoId="kkxQYilNuqg"
-                width="100%"
-                height="250px"
-              />
-            </div>
-            <h6>Cloud & Cyber Security</h6>
-            <p>Top 10: The technology world’s most influential CTOs</p>
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
-};
-
-const MostViewed = () => {
-  return (
-    <div>
-      <SectionTitle title="most viewed" />
-      <Row>
-        <Col lg={6}>
-          <div className={styles.mostViewedCard}>
-            <Row>
-              <Col lg={5} md={6}>
-                <div className={styles.videoContainer}>
-                  <YouTubePlayer
-                    videoId="Gcf5My4ALxo"
-                    width="250px"
-                    height="150px"
-                  />
-                </div>
-              </Col>
-              <Col lg={7} md={6}>
-                <div>
-                  <h6>Malaria & Neglected Diseases </h6>
-                  <p>
-                    Africa’s ‘Noma Belt:’ Health Workers Tackle the Deadly NTD
-                    in Nigeria’s Remote North
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </div>
-          <div className={styles.mostViewedBorder}></div>
-          <div className={styles.mostViewedCard}>
-            <Row>
-              <Col lg={5} md={6}>
-                <div className={styles.videoContainer}>
-                  <YouTubePlayer
-                    videoId="MOv6sAmKOSM"
-                    width="250px"
-                    height="150px"
-                  />
-                </div>
-              </Col>
-              <Col lg={7} md={6}>
-                <div>
-                  <h6>Non-Communicable Diseases</h6>
-                  <p>
-                    A&quot;Tsunami&quot;of Chronic Disease Challenges Confronts
-                    Health Sector Response to Humanitarian Crises
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-        <Col lg={6}>
-          <div className={styles.mostViewedCard}>
-            <Row>
-              <Col lg={5} md={6}>
-                <div className={styles.videoContainer}>
-                  <YouTubePlayer
-                    videoId="o8-2_Z8CMbg"
-                    width="250px"
-                    height="150px"
-                  />
-                </div>
-              </Col>
-              <Col lg={7} md={6}>
-                <div>
-                  <h6>Malaria & Neglected Diseases</h6>
-                  <p>
-                    Africa’s ‘Noma Belt:’ Health Workers Tackle the Deadly NTD
-                    in Nigeria’s Remote North
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </div>
-          <div className={styles.mostViewedBorder}></div>
-          <div className={styles.mostViewedCard}>
-            <Row>
-              <Col lg={5} md={6}>
-                <div className={styles.videoContainer}>
-                  <YouTubePlayer
-                    videoId="YAKZ2jhG7T8"
-                    width="250px"
-                    height="150px"
-                  />
-                </div>
-              </Col>
-              <Col lg={7} md={6}>
-                <div>
-                  <h6>Non-Communicable Diseases</h6>
-                  <p>
-                    A &quot;Tsunami&quot; of Chronic Disease Challenges
-                    Confronts Health Sector Response to Humanitarian Crises
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
-};
-
-const FirstRowCard = () => {
-  return (
-    <div>
-      <div className={styles.textualViewAll}>
-        <SectionTitle title="Textual interviews" />
-        <Link href="/webinar/textual-interviews">
-          View All <MdKeyboardArrowRight />
-        </Link>
+        </div>
       </div>
-      <p>
-        Read the latest posts from experts at Knowmed Health Publishing covering
-        a variety of health topics and perspectives on medical news.
-      </p>
-      <Row>
-        <Col lg={9}>
-          <div className={styles.textualCard}>
-            <div className={styles.textualImg}>
-              <Image src={cardImg1} alt="card" />
-            </div>
-            <div className={styles.textualContent}>
-              <h4>Pandemic Agreement</h4>
-              <h1>Greg Wilson</h1>
-              <h2>
-                Microsoft worldwide Public Sector&quot;s Chief Technology
-                Officer for Government
-              </h2>
-              <p>
-                Microsoft Government CTO on the company&apos;s expansive role in
-                AI adoption, Cloud migration, ethical governance, and
-                cybersecurity within the public sector
-              </p>
-              <MgButton>
-                Read More <MdKeyboardArrowRight />
-              </MgButton>
-            </div>
-          </div>
-        </Col>
-        <Col lg={3}>
-          <div className={styles.textualAd}>Ad</div>
-        </Col>
-      </Row>
-    </div>
-  );
-};
-
-const SecondRowCard = () => {
-  return (
-    <div className={styles.textualCardSecond}>
-      <Row>
-        <Col lg={6}>
-          <div className={styles.textualCard2}>
-            <Row>
-              <Col lg={5} md={6}>
-                <div className={styles.textualImg2}>
-                  <Image src={cardImg2} alt="card" />
-                </div>
-              </Col>
-              <Col lg={7} md={6}>
-                <div className={styles.textualContent2}>
-                  <h1>Garrett Olson</h1>
-                  <h4>CTO at Michigan Medicine</h4>
-                  <h2>
-                    Michigan Medicine CTO Dr Tim Calahan on how the Organisation
-                    is Implementing a Cloud Transformation, Moving its Epic
-                    Systems off-prem for key Benefits
-                  </h2>
-                  <MgButton>
-                    Read More <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-        <Col lg={6}>
-          <div className={styles.textualCard2}>
-            <Row>
-              <Col lg={5} md={6}>
-                <div className={styles.textualImg}>
-                  <Image src={cardImg3} alt="card" />
-                </div>
-              </Col>
-              <Col lg={7} md={6}>
-                <div className={styles.textualContent2}>
-                  <h1>Remi Lavergne</h1>
-                  <h4>Head of IT & Compliance at GFS</h4>
-                  <h2>
-                    Remi Lavergne, Head of IT Compliance at Global Freight
-                    Services Reflects on a 25 Year Career in Logistics
-                  </h2>
-
-                  <MgButton>
-                    Read More <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
-};
-
-const ThirdRowCard = () => {
-  return (
-    <div className={styles.textualCardThird}>
-      <Row>
-        <Col lg={4}>
-          <div className={styles.textualCard3}>
-            <Row>
-              <Col lg={5}>
-                <div className={styles.textualImg3}>
-                  <Image src={cardImg4} alt="card" />
-                </div>
-              </Col>
-              <Col lg={7}>
-                <div className={styles.textualContent3}>
-                  <h3>Cindy Perettie</h3>
-                  <p>Executive Vice President and Global Head of Kite</p>
-                  <h5>
-                    Bringing Business and Global Health, Cindy Perettie,
-                    Executive VP of Kite, Shares a Journey of...
-                  </h5>
-                  <MgButton>
-                    Read More <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-        <Col lg={4}>
-          <div className={styles.textualCard3}>
-            <Row>
-              <Col lg={5}>
-                <div className={styles.textualImg3}>
-                  <Image src={cardImg5} alt="card" />
-                </div>
-              </Col>
-              <Col lg={7}>
-                <div className={styles.textualContent3}>
-                  <h3>Chris McDonald</h3>
-                  <p>Global Head of Technical Operations</p>
-                  <h5>
-                    Chris McDonald, Global Head of Technical Operations, on his
-                    Journey to Lead a Sustainable ...
-                  </h5>
-                  <MgButton>
-                    Read More <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-        <Col lg={4}>
-          <div className={styles.textualCard3}>
-            <Row>
-              <Col lg={5}>
-                <div className={styles.textualImg3}>
-                  <Image src={cardImg6} alt="card" />
-                </div>
-              </Col>
-              <Col lg={7}>
-                <div className={styles.textualContent3}>
-                  <h3>Khang Pham Ngoc</h3>
-                  <p>Chief Financial Officer at Home Credit Vietnam</p>
-                  <h5>
-                    Home Credit Vietnam CFO Khang Pham Ngoc Tells us how he Came
-                    into his Role, and how…
-                  </h5>
-                  <MgButton>
-                    Read More <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-      </Row>
-    </div>
+    </Link>
   );
 };
 
 const Webinar = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.heroWrapper}>
-        <Container>
-          <div className={styles.hero}>
-            <div className={styles.heroContent}>
-              <h1>Care for one</h1>
-              <h1>Outcomes for all</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-                bibendum pellentesque rutrum. Vestibulum at tellus ultrices,
-                sollicitudin
-              </p>
+      <Container>
+        <div className={styles.viewAll}>
+          <SectionTitle title="Webinars" />
+          <Link href="#">
+            View All <MdKeyboardArrowRight />
+          </Link>
+        </div>
+        <div className={styles.cardWrapper}>
+          <div>
+            <div className={styles.featuredCardSection}>
+              <FeaturedCard />
             </div>
-            <div className={styles.heroImage}>
-              <div className={styles.imageContainer}>
-                <Image
-                  className={styles.ellipseImg}
-                  src={ellipseImg}
-                  alt="ellipse"
-                />
-                <Image
-                  className={styles.heroImage1}
-                  src={heroImg1}
-                  alt="image"
-                />
-                <Image
-                  className={styles.heroImage2}
-                  src={heroImg2}
-                  alt="image"
-                />
-                <Image
-                  className={styles.heroImage3}
-                  src={heroImg3}
-                  alt="image"
-                />
+            <div className={styles.webinarCardCotainer}>
+              <div>
+                <WebinarCard />
+              </div>
+              <div>
+                <WebinarCard />
+              </div>
+              <div>
+                <WebinarCard />
               </div>
             </div>
           </div>
-        </Container>
-      </div>
-      <div className={styles.interview}>
-        <Container>
-          <FirstRowInterview />
-          <SecondRowInterview />
-        </Container>
-      </div>
-      <div className={styles.mostViewed}>
-        <Container>
-          <div>
-            <MostViewed />
-          </div>
-        </Container>
-      </div>
-      <div className={styles.textualInterview}>
-        <Container>
-          <div>
-            <FirstRowCard />
-            <SecondRowCard />
-            <ThirdRowCard />
-          </div>
-        </Container>
-      </div>
+          <div className={styles.webinarAds}>Ads</div>
+        </div>
+      </Container>
     </div>
   );
 };
+
 export default Webinar;
