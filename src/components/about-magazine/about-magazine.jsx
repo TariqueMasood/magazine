1ccc15541c1ce2db0d8d0ccdb324ed714b3a6971
AkramAltaf <41393849+AkramAltaf@users.noreply.github.com>
2024-04-09 03:15:03 +0530

    CSE-502:conflict resolve

--- src/components/about-magazine/about-magazine.jsx
+++ src/components/about-magazine/about-magazine.jsx
@@ -1,13 +1,12 @@
 import { Col, Container, Row } from "react-bootstrap";
 import styles from "./about-magazine.module.css";
 import Image from "next/image";
-import aboutImg from "../../../public/images/about-img.svg";
+import aboutImg from "../../../public/images/about-img2.png";
 import icon1 from "../../../public/images/book-open-icon.png";
-import icon2 from "../../../public/images/book-close-icon.png";
 import icon3 from "../../../public/images/man-sitting-icon.png";
 import icon4 from "../../../public/images/hand-open-icon.png";
 import icon5 from "../../../public/images/text-icon.png";
-import icon6 from "../../../public/images/sound-icon.png";
+import SectionBorder from "../section-border/section-border";
 
 function ServiceStep(props) {
   return (
@@ -19,7 +18,7 @@ function ServiceStep(props) {
         <h5 className={styles.stepHeader}>{props.title}</h5>
         <p className={styles.stepText}>{props.desc}</p>
       </div>
-      {props.connector ? <div className={styles.dottedConnector} /> : null}
+      {/* {props.connector ? <div className={styles.dottedConnector} /> : null} */}
     </div>
   );
 }
@@ -28,82 +27,73 @@ const AboutMagazine = () => {
   return (
     <div className={styles.wrapper}>
       <Container>
-        <Row>
-          <Col lg={6}>
-            <div className={styles.serviceWrapper}>
-              <h1 className={styles.aboutHeader}>
-                Innovative projects, impactful leaders, captivating
-                storytelling.
-              </h1>
-              <div className={styles.serviceStepWrapper}>
-                <ServiceStep
-                  icon={icon1}
-                  title="Project stories"
-                  desc="Transformative projects, diverse stories."
-                  connector={true}
-                />
-                <ServiceStep
-                  icon={icon2}
-                  title="Case studies"
-                  desc="Industries' project success strategies for you."
-                  connector={true}
-                />
-                <ServiceStep
-                  icon={icon3}
-                  title="Interview"
-                  desc="Global projects, leaders, challenges, learning."
-                  connector={true}
-                />
-                <ServiceStep
-                  icon={icon4}
-                  title="Success stories"
-                  desc="Secrets of victorious project leaders revealed."
-                  connector={true}
-                />
-                <ServiceStep
-                  icon={icon5}
-                  title="Expert opinions"
-                  desc="Experts sharing project success strategies."
-                />
-                <ServiceStep
-                  icon={icon6}
-                  title="industry news"
-                  desc="Dynamic news on projects, strategies, industries."
-                />
-              </div>
-            </div>
-          </Col>
-          <Col lg={6}>
-            <div>
-              <p className={styles.aboutText}>
-                Our digital magazine is more than just a collection of articles,
-                that stands out from the rest by providing our readers with
-                unparalleled insights on projects and project leadership. We
-                deliver value that makes you stand out from the crowd.
-              </p>
-              <div className={styles.imgContainer}>
-                <Image
-                  src={aboutImg}
-                  alt="about-magazine"
-                  className={styles.imgCss}
-                />
-                <div className={styles.services}>
-                  <div className={styles.serviceContainer}>
-                    <span className={styles.count}>100+</span>
-                    <span className={styles.serviceText}>project stories</span>
-                  </div>
-                  <div className={styles.divider}></div>
-                  <div className={styles.serviceContainer}>
-                    <span className={styles.count}>15+</span>
-                    <span className={styles.serviceText}>
-                      Leaders interviews
-                    </span>
-                  </div>
+        <div>
+          <h1 className={styles.aboutHeader}>
+            Innovative projects, <br /> impactful leaders, captivating
+            storytelling.
+          </h1>
+          <p className={styles.aboutText}>
+            Our digital magazine is more than just a collection of articles,
+            that stands out from the rest by providing our readers with
+            unparalleled insights on projects and project leadership. We deliver
+            value that makes you stand out from the crowd.
+          </p>
+          <div className={styles.magazineBorderWrapper}>
+            <SectionBorder />
+          </div>
+          <div className={styles.serviceStepWrapper}>
+            <Row>
+              <Col lg={6} md={6}>
+                <div className={styles.serviceStepIcon}>
+                  <ServiceStep
+                    icon={icon1}
+                    title="Project stories"
+                    desc="Join the journey of the projects that are transforming the world, with diverse and fascinating stories from across a range of domains."
+                    connector={true}
+                  />
                 </div>
-              </div>
-            </div>
-          </Col>
-        </Row>
+              </Col>
+              <Col lg={6} md={6}>
+                <div className={styles.serviceStepIcon}>
+                  <ServiceStep
+                    icon={icon3}
+                    title="Interview"
+                    desc="Explore challenging and rewarding projects worldwide, learning from experienced leaders who made them happen."
+                    connector={true}
+                  />
+                </div>
+              </Col>
+            </Row>
+            <Row>
+              <Col lg={6} md={6}>
+                <div className={styles.serviceStepIcon}>
+                  <ServiceStep
+                    icon={icon4}
+                    title="Success stories"
+                    desc="Optimized for mobile devices, ensuring seamless access on the go for readers everywhere."
+                    connector={true}
+                  />
+                </div>
+              </Col>
+              <Col lg={6} md={6}>
+                <div className={styles.serviceStepIcon}>
+                  <ServiceStep
+                    icon={icon5}
+                    title="Expert opinions"
+                    desc="Insights of how experts tackle the different facets and hurdles of delivering successful projects in the real world."
+                  />
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            src={aboutImg}
+            alt="about-magazine"
+            className={styles.imgCss}
+          />
+        </div>
       </Container>
     </div>
   );
