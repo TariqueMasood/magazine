a299d16573bbc5804a5822e185f4e783ef0af410
AkramAltaf <41393849+AkramAltaf@users.noreply.github.com>
2024-04-16 12:45:41 +0530

    CSE-502:style changed

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,10 +1,21 @@
+import NewsLetter from "@/components/news-letter/news-letter";
 import styles from "./blogs.module.css";
 import RecentBlog from "@/components/recent-blog/recent-blog";
 import TechnologyBlog from "@/components/technology-blog/technology-blog";
+import SectionHeader from "@/components/section-header/section-header";
+import Subscribe from "@/components/subscribe/subscribe";
 
 const BlogPage = async () => {
   return (
     <div className={styles.wrapper}>
+      <div className="text-center pt-5">
+        <SectionHeader
+          title="News Letter"
+          subTitle="Subscribe to monthly creative magazine"
+          desc="Get notified to our exclusive releases & events."
+        />
+        <Subscribe />
+      </div>
       <RecentBlog />
       <TechnologyBlog />
     </div>
--- src/components/recent-blog/recent-blog.jsx
+++ src/components/recent-blog/recent-blog.jsx
@@ -10,7 +10,14 @@ const RecentCard = () => {
   return (
     <div>
       <div className={styles.cardImg}>
-        <Image src={cardImg1} alt="card" />
+        <Image
+          src={cardImg1}
+          alt="card"
+          width={350}
+          height={100}
+          layout="responsive"
+          objectFit="cover"
+        />
       </div>
       <div className={styles.cardContent}>
         <div className={styles.cardName}>
@@ -28,7 +35,7 @@ const RecentCard = () => {
           sollicitudin arcu tempus, condimentum libero. Vestibulum non purus
           quam. Nam ut lacus magna.
         </p>
-        <p className={styles.cardDate}>19-Sep-2021</p>
+        {/* <p className={styles.cardDate}>19-Sep-2021</p> */}
       </div>
     </div>
   );
@@ -37,33 +44,33 @@ const RecentCard = () => {
 const RecentCard2 = () => {
   return (
     <div className={styles.cardWrapper}>
-      <Row>
-        <Col lg={4}>
-          <div className={styles.card2Image}>
-            <Image src={cardImg2} alt="card" />
+      <div className={styles.card2Image}>
+        <Image
+          src={cardImg1}
+          alt="card"
+          width={350}
+          height={300}
+          layout="responsive"
+        />
+      </div>
+      <div className={styles.recentBlogContent}>
+        <div className={styles.cardName}>
+          <div className={styles.category}>Diseases & Conditions</div>
+          <div className={styles.shareIcon}>
+            <Image src={shareIcon} alt="icon" />
           </div>
-        </Col>
-        <Col lg={8}>
-          <div className={styles.recentBlogContent}>
-            <div className={styles.cardName}>
-              <div className={styles.category}>Diseases & Conditions</div>
-              <div className={styles.shareIcon}>
-                <Image src={shareIcon} alt="icon" />
-              </div>
-            </div>
-            <h4 className={styles.cardTitle}>
-              Consider day care surgery as default for all elective...
-            </h4>
-            <p className={styles.cardDesc}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-              bibendum pellentesque rutrum. Vestibulum at tellus ultrices,
-              sollicitudin arcu tempus, condimentum libero. Vestibulum non purus
-              quam. Nam ut lacus magna.
-            </p>
-            <p className={styles.cardDate}>24-Mar-2022</p>
-          </div>
-        </Col>
-      </Row>
+        </div>
+        <h4 className={styles.cardTitle}>
+          Consider day care surgery as default for all elective...
+        </h4>
+        <p className={styles.cardDesc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+          bibendum pellentesque rutrum. Vestibulum at tellus ultrices,
+          sollicitudin arcu tempus, condimentum libero. Vestibulum non purus
+          quam. Nam ut lacus magna.
+        </p>
+        {/* <p className={styles.cardDate}>24-Mar-2022</p> */}
+      </div>
     </div>
   );
 };
@@ -71,7 +78,9 @@ const RecentCard2 = () => {
 const RecentBlogCard = () => {
   return (
     <div className={styles.wrapper}>
-      <SectionTitle title="RECENT BLOGS" />
+      <div className="pb-3">
+        <SectionTitle title="RECENT BLOGS" />
+      </div>
       <Row>
         <Col sm={12} md={12} lg={6}>
           <RecentCard />
