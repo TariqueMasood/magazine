00a985234b6d6b0d4fd49effd7cc67aa58c60571
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-04-21 12:16:35 +0530

    ECE-402: fix

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,9 +1,6 @@
-import NewsLetter from "@/components/news-letter/news-letter";
 import styles from "./blogs.module.css";
 import RecentBlog from "@/components/recent-blog/recent-blog";
 import TechnologyBlog from "@/components/technology-blog/technology-blog";
-import SectionHeader from "@/components/section-header/section-header";
-import Subscribe from "@/components/subscribe/subscribe";
 
 const BlogPage = async () => {
   return (
