d79f60a430ba3e7112b6511f9fdf3451e71dd939
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-04-22 12:15:30 +0530

    ECE-402: eslint rule disabled

--- src/components/blog-tabs/blog-tabs.jsx
+++ src/components/blog-tabs/blog-tabs.jsx
@@ -5,62 +5,61 @@ import { useEffect, useState } from "react";
 import { fetchData } from "@/utils/api";
 
 const BlogTabs = (props) => {
-  let [tabsData, setTabsData] = useState([])
-  let [activeTab, setActiveTab] = useState('')
-  let [blogsData, setBlogsData] = useState([])
-  let [blogsTempData, setBlogsTempData] = useState([])
-  let [prevValue, setPrevValue] = useState(null)
-  let [nextValue, setNextValue] = useState(null)
+  let [tabsData, setTabsData] = useState([]);
+  let [activeTab, setActiveTab] = useState("");
+  let [blogsData, setBlogsData] = useState([]);
+  let [blogsTempData, setBlogsTempData] = useState([]);
+  let [prevValue, setPrevValue] = useState(null);
+  let [nextValue, setNextValue] = useState(null);
 
-  const capitalizeFirstLetter = (string) => string.charAt(0).toUpperCase() + string.slice(1);
+  const capitalizeFirstLetter = (string) =>
+    string.charAt(0).toUpperCase() + string.slice(1);
 
   let getTabData = async () => {
     const tabsData = await fetchData("categories");
-    setTabsData(tabsData.results)
-    getBlogsData('blogs', tabsData.results)
-
-  }
+    setTabsData(tabsData.results);
+    getBlogsData("blogs", tabsData.results);
+  };
 
   let getBlogsData = async (url, tab) => {
-    let blogsDatas = null
+    let blogsDatas = null;
     blogsDatas = await fetchData(url);
-    console.log("data come", blogsDatas)
-    setPrevValue(blogsDatas.previous)
-    setNextValue(blogsDatas.next)
+    console.log("data come", blogsDatas);
+    setPrevValue(blogsDatas.previous);
+    setNextValue(blogsDatas.next);
 
-    setBlogsData(blogsDatas)
-    setBlogsTempData(blogsData)
-    if(activeTab === "")
-    {
-      handleTabChange(tab[0].name, blogsDatas)
+    setBlogsData(blogsDatas);
+    setBlogsTempData(blogsData);
+    if (activeTab === "") {
+      handleTabChange(tab[0].name, blogsDatas);
+    } else {
+      handleTabChange(activeTab, blogsDatas);
     }
-    else{
-      handleTabChange(activeTab, blogsDatas)
-
-    }
-
-  }
+  };
 
   let handleTabChange = (key, data = []) => {
-    let  filteredData = []
-    setActiveTab(key)
+    let filteredData = [];
+    setActiveTab(key);
     if (data.length === 0) {
-      filteredData = blogsData.results.filter(item => item.category.name.toLowerCase().trim() === key.toLowerCase().trim());
+      filteredData = blogsData.results.filter(
+        (item) =>
+          item.category.name.toLowerCase().trim() === key.toLowerCase().trim()
+      );
+    } else {
+      filteredData = data.results.filter(
+        (item) =>
+          item.category.name.toLowerCase().trim() === key.toLowerCase().trim()
+      );
     }
-    else {
-      filteredData = data.results.filter(item => item.category.name.toLowerCase().trim() === key.toLowerCase().trim());
+    let temp = { ...blogsData };
 
-    }
-    let temp = {...blogsData}
-
-    temp.results = filteredData
+    temp.results = filteredData;
     if (filteredData.length !== 0) {
-      setBlogsTempData(temp)
+      setBlogsTempData(temp);
+    } else {
+      setBlogsTempData([]);
     }
-    else {
-      setBlogsTempData([])
-    }
-  }
+  };
 
   let handlePrev = async (url) => {
     console.log("url", url, blogsTempData);
@@ -69,59 +68,71 @@ const BlogTabs = (props) => {
     let pathname = parsedUrl.pathname;
     pathname = pathname.slice(4).substring(1);
     const newurl = parsedUrl.search;
-    getBlogsData(pathname + newurl, "")
-  }
+    getBlogsData(pathname + newurl, "");
+  };
 
   let handleNext = (url) => {
     const parsedUrl = new URL(url);
     let pathname = parsedUrl.pathname;
     pathname = pathname.slice(4).substring(1);
     const newurl = parsedUrl.search;
-    getBlogsData(pathname + newurl, "")
-  }
+    getBlogsData(pathname + newurl, "");
+  };
 
   useEffect(() => {
-    getTabData()
-  }, [])
+    getTabData();
+  }, []);
 
   return (
     <>
-      <Tabs 
-        defaultActiveKey={tabsData && tabsData.length > 0 ? tabsData[0].name.toLowerCase() : "news"}
+      <Tabs
+        defaultActiveKey={
+          tabsData && tabsData.length > 0
+            ? tabsData[0].name.toLowerCase()
+            : "news"
+        }
         id="uncontrolled-tab-example"
         className="mb-3"
         onSelect={(key) => handleTabChange(key, [])}
       >
-
-
-        {tabsData && tabsData.map((tab) => (
-          <Tab key={tab.id} eventKey={tab.name.toLowerCase()} title={capitalizeFirstLetter(tab.name)}>
-            <Row>
-              <Row className="justify-content-center mb-4">
-                <Col md={1}>
-                  <Button variant="primary" onClick={() => handlePrev(blogsData.previous)} disabled={prevValue === null}>
-                    &lt; Prev
-                  </Button>
-                </Col>
-                <Col md={10}>
-
-                </Col>
-                <Col md={1}>
-                  <Button variant="primary" onClick={() => handleNext(blogsData.next)} disabled={nextValue === null}>
-                    Next &gt;
-                  </Button>
-                </Col>
+        {tabsData &&
+          tabsData.map((tab) => (
+            <Tab
+              key={tab.id}
+              eventKey={tab.name.toLowerCase()}
+              title={capitalizeFirstLetter(tab.name)}
+            >
+              <Row>
+                <Row className="justify-content-center mb-4">
+                  <Col md={1}>
+                    <Button
+                      variant="primary"
+                      onClick={() => handlePrev(blogsData.previous)}
+                      disabled={prevValue === null}
+                    >
+                      &lt; Prev
+                    </Button>
+                  </Col>
+                  <Col md={10}></Col>
+                  <Col md={1}>
+                    <Button
+                      variant="primary"
+                      onClick={() => handleNext(blogsData.next)}
+                      disabled={nextValue === null}
+                    >
+                      Next &gt;
+                    </Button>
+                  </Col>
+                </Row>
+                {blogsTempData.results &&
+                  blogsTempData.results.map((data) => (
+                    <Col key={data.id} md={6} lg={4}>
+                      <BlogCard data={data} />
+                    </Col>
+                  ))}
               </Row>
-              {blogsTempData.results && blogsTempData.results.map((data) =>
-              (
-                <Col key={data.id} md={6} lg={4}>
-                  <BlogCard data={data} />
-                </Col>
-              )
-              )}
-            </Row>
-          </Tab>
-        ))}
+            </Tab>
+          ))}
       </Tabs>
     </>
     // <Tab.Container id="left-tabs-example" defaultActiveKey="first">
