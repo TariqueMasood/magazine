9903f4eb8f4dbf3ba0bd73399315b0dd82ddaea6
TariqueMasood <64158034+TariqueMasood@users.noreply.github.com>
2024-04-28 02:53:53 +0530

    ECE-402: interview api integration

--- src/app/interview/page.jsx
+++ src/app/interview/page.jsx
@@ -1,24 +1,14 @@
 "use client";
 
 import MgButton from "@/components/button/button";
-import heroImg1 from "../../../public/images/wb-img1.png";
-import heroImg2 from "../../../public/images/wb-img2.png";
-import heroImg3 from "../../../public/images/wb-img3.png";
 import ellipseImg from "../../../public/images/interview-circle-img.png";
 import styles from "./interview.module.css";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import Image from "next/image";
 import SectionTitle from "@/components/section-title/section-title";
 import { MdKeyboardArrowRight } from "react-icons/md";
-import cardImg1 from "../../../public/images/textual-interview-img1.png";
-import cardImg2 from "../../../public/images/textual-interview-img2.png";
-import cardImg3 from "../../../public/images/textual-interview-img3.png";
-import cardImg4 from "../../../public/images/textual-interview-img4.png";
-import cardImg5 from "../../../public/images/textual-interview-img5.png";
-import cardImg6 from "../../../public/images/textual-interview-img6.png";
 import YouTube from "react-youtube";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 import { fetchData } from "@/utils/api";
 import BoxAds from "@/components/ads/box-ads";
 
@@ -34,89 +24,46 @@ const YouTubePlayer = ({ videoId, width, height }) => {
   );
 };
 
-const VideoTextualInterview = () => {
+const VideoTextualInterview = async () => {
+  const interviews = await fetchData("interviews/?limit=2");
   return (
-    <div>
-      <SectionTitle title="Featured Interviews" />
-      <Row className="mt-3">
-        <Col lg={6}>
-          {/* <div className={styles.videoTextual}>
-            <div>
-              <div className={styles.videoTextualContainer}>
-                <div className={styles.YouTubeVideo}>
-                  <YouTubePlayer
-                    videoId="meT67QQqrIw"
-                    width="100%"
-                    height="250px"
-                  />
-                </div>
-              </div>
-              <div className={styles.videoTextualContainer}>
-                <h3>Exclusive Video: Red Hat at MWC 2024</h3>
-                <h6>Digital Transformation</h6>
-                <p>
-                  We speak with Ben Panic, Vice President of Red Hat’s Global
-                  Telco Centre of Excellence, about the company’s MWC 2024
-                  announcements and future telco trends
-                </p>
-              </div>
-            </div>
-          </div> */}
-          <div className={styles.videoTextualCard}>
-            <Row>
-              <Col lg={6} md={6}>
-                <div className={styles.videoTextualImg}>
-                  <Image src={cardImg2} alt="card" />
-                </div>
-              </Col>
-              <Col lg={6} md={6}>
-                <div className={styles.videoTextualContent}>
-                  <h1>Garrett Olson</h1>
-                  <h4>Head of IT & Compliance at GFS</h4>
-                  <p>
-                    Remi Lavergne, Head of IT Compliance at Global Freight
-                    Services Reflects on a 25 Year Career in Logistics With
-                    demand only set to grow, Serverfarm continues to pioneer
-                    sustainable, innovative and efficient data centre solutions
-                    as part of its long...
-                  </p>
-                  <MgButton>
-                    Read Full Interview <MdKeyboardArrowRight />
-                  </MgButton>
+    <>
+      {interviews?.results.is_feature && (
+        <div>
+          <SectionTitle title="Featured Interviews" />
+          <Row className="mt-3">
+            {interviews?.results.map((interview) => (
+              <Col lg={6} key={interview.id}>
+                <div className={styles.videoTextualCard}>
+                  <Row>
+                    <Col lg={6} md={6}>
+                      <div className={styles.videoTextualImg}>
+                        <Image
+                          src={interview.profile_pic}
+                          alt="profile-pic"
+                          width={130}
+                          height={100}
+                        />
+                      </div>
+                    </Col>
+                    <Col lg={6} md={6}>
+                      <div className={styles.videoTextualContent}>
+                        <h1>{interview.title}</h1>
+                        <h4>{interview.position}</h4>
+                        <p>{interview.subtitle}</p>
+                        <MgButton>
+                          Read Full Interview <MdKeyboardArrowRight />
+                        </MgButton>
+                      </div>
+                    </Col>
+                  </Row>
                 </div>
               </Col>
-            </Row>
-          </div>
-        </Col>
-        <Col lg={6}>
-          <div className={styles.videoTextualCard}>
-            <Row>
-              <Col lg={6} md={6}>
-                <div className={styles.videoTextualImg}>
-                  <Image src={cardImg2} alt="card" />
-                </div>
-              </Col>
-              <Col lg={6} md={6}>
-                <div className={styles.videoTextualContent}>
-                  <h1>Garrett Olson</h1>
-                  <h4>Head of IT & Compliance at GFS</h4>
-                  <p>
-                    Remi Lavergne, Head of IT Compliance at Global Freight
-                    Services Reflects on a 25 Year Career in Logistics With
-                    demand only set to grow, Serverfarm continues to pioneer
-                    sustainable, innovative and efficient data centre solutions
-                    as part of its long...
-                  </p>
-                  <MgButton>
-                    Read Full Interview <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-      </Row>
-    </div>
+            ))}
+          </Row>
+        </div>
+      )}
+    </>
   );
 };
 
@@ -327,24 +274,8 @@ const MostViewed = () => {
   );
 };
 
-export const FirstRowCard = () => {
-  let [textualInterviews, setTextualInterviews] = useState(null);
-
-  let getTextualInterviewsData = async (url) => {
-    let textualInterviewsData = await fetchData(url);
-    let list = textualInterviewsData.results.filter(
-      (items) => items.is_feature === true && items.is_trending === true
-    );
-    if (list.length !== 0) {
-      setTextualInterviews(list);
-    } else {
-      setTextualInterviews(null);
-    }
-  };
-
-  useEffect(() => {
-    getTextualInterviewsData("interviews");
-  }, []);
+export const FirstRowCard = async () => {
+  const interviews = await fetchData("interviews");
 
   return (
     <div>
@@ -364,9 +295,9 @@ export const FirstRowCard = () => {
           <Row>
             <div className={styles.textualCard}>
               <div className={styles.textualImg}>
-                {textualInterviews && (
+                {interviews && (
                   <Image
-                    src={textualInterviews.results[0].profile_pic}
+                    src={interviews.results[0].profile_pic}
                     alt="card"
                     width={130}
                     height={100}
@@ -374,24 +305,9 @@ export const FirstRowCard = () => {
                 )}
               </div>
               <div className={styles.textualContent}>
-                <h4>
-                  {textualInterviews && textualInterviews.results[0].title}
-                </h4>
-                <h1>
-                  {textualInterviews &&
-                    textualInterviews.results[0].interviewee}{" "}
-                  ({textualInterviews && textualInterviews.results[0].position})
-                </h1>
-                <h2>
-                  {textualInterviews && textualInterviews.results[0].subtitle}
-                </h2>
-                {textualInterviews && (
-                  <div
-                    dangerouslySetInnerHTML={{
-                      __html: textualInterviews.results[0].description,
-                    }}
-                  />
-                )}
+                <h4>{interviews && interviews.results[0].title}</h4>
+                <h1>{interviews && interviews.results[0].position}</h1>
+                <h2>{interviews && interviews.results[0].subtitle}</h2>
                 <MgButton>
                   Read More <MdKeyboardArrowRight />
                 </MgButton>
@@ -408,209 +324,72 @@ export const FirstRowCard = () => {
 };
 
 export const SecondRowCard = async () => {
-  const interviews = await fetchData("interviews");
+  const interviews = await fetchData("interviews/?limit=2");
   return (
     <div className={styles.textualCardSecond}>
       <Row>
-        <Col lg={6}>
-          <div className={styles.textualCard2}>
-            <Row>
-              <Col lg={5} md={6}>
-                <div className={styles.textualImg2}>
-                  <Image src={cardImg2} alt="card" />
-                </div>
-              </Col>
-              <Col lg={7} md={6}>
-                <div className={styles.textualContent2}>
-                  <h1>Garrett Olson</h1>
-                  <h4>CTO at Michigan Medicine</h4>
-                  <h2>
-                    Michigan Medicine CTO Dr Tim Calahan on how the Organisation
-                    is Implementing a Cloud Transformation, Moving its Epic
-                    Systems off-prem for key Benefits
-                  </h2>
-                  <MgButton>
-                    Read More <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
-        <Col lg={6}>
-          <div className={styles.textualCard2}>
-            <Row>
-              <Col lg={5} md={6}>
-                <div className={styles.textualImg}>
-                  <Image src={cardImg3} alt="card" />
-                </div>
-              </Col>
-              <Col lg={7} md={6}>
-                <div className={styles.textualContent2}>
-                  <h1>Remi Lavergne</h1>
-                  <h4>Head of IT & Compliance at GFS</h4>
-                  <h2>
-                    Remi Lavergne, Head of IT Compliance at Global Freight
-                    Services Reflects on a 25 Year Career in Logistics
-                  </h2>
-
-                  <MgButton>
-                    Read More <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col>
+        {interviews?.results &&
+          interviews?.results.map((interview) => (
+            <Col lg={6} key={interview.id}>
+              <div className={styles.textualCard2}>
+                <Row>
+                  <Col lg={5} md={6}>
+                    <div className={styles.textualImg2}>
+                      <Image
+                        src={interview.profile_pic}
+                        alt="profile-pic"
+                        width={130}
+                        height={100}
+                      />
+                    </div>
+                  </Col>
+                  <Col lg={7} md={6}>
+                    <div className={styles.textualContent2}>
+                      <h1>{interview.title}</h1>
+                      <h4>{interview.position}</h4>
+                      <h2>{interview.subtitle}</h2>
+                      <MgButton>
+                        Read More <MdKeyboardArrowRight />
+                      </MgButton>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+            </Col>
+          ))}
       </Row>
     </div>
   );
 };
 
-const ThirdRowCard = () => {
-  let [prevValue, setPrevValue] = useState(null);
-  let [nextValue, setNextValue] = useState(null);
-  let [textualInterviews, setTextualInterviews] = useState([]);
-
-  let getTextualInterviewsData = async (url) => {
-    let textualInterviewsData = await fetchData(url);
-    let list = textualInterviewsData.results.filter(
-      (items) => items.is_feature === false && items.is_trending === false
-    );
-    textualInterviewsData.results = list;
-    if (list.length !== 0) {
-      setTextualInterviews(textualInterviewsData);
-      setPrevValue(textualInterviewsData.previous);
-      setNextValue(textualInterviewsData.next);
-    } else {
-      setTextualInterviews([]);
-      setPrevValue(null);
-      setNextValue(null);
-    }
-  };
-  let handlePrev = async (url) => {
-    const parsedUrl = new URL(url);
-    let pathname = parsedUrl.pathname;
-    pathname = pathname.slice(4).substring(1);
-    const newurl = parsedUrl.search;
-    getMagazineData(pathname + newurl);
-  };
-
-  let handleNext = (url) => {
-    console.log("url", url);
-    const parsedUrl = new URL(url);
-    let pathname = parsedUrl.pathname;
-    pathname = pathname.slice(4).substring(1);
-    const newurl = parsedUrl.search;
-    getMagazineData(pathname + newurl);
-  };
-  useEffect(() => {
-    getTextualInterviewsData("interviews");
-  }, []);
+const InterviewBanner = () => {
   return (
-    <div className={styles.textualCardThird}>
-      <Row className="justify-content-center mb-4">
-        <Col md={2}>
-          <Button
-            variant="primary"
-            onClick={() => handlePrev(textualInterviews.previous)}
-            disabled={prevValue === null}
-          >
-            &lt; Prev
-          </Button>
-        </Col>
-        <Col md={8}></Col>
-        <Col md={2}>
-          <Button
-            variant="primary ms-md-5"
-            onClick={() => handleNext(textualInterviews.next)}
-            disabled={nextValue === null}
-          >
-            Next &gt;
-          </Button>
-        </Col>
-      </Row>
-      <Row>
-        {textualInterviews?.results?.map((val, ind) => (
-          <Col key={val.id} lg={4}>
-            <div className={styles.textualCard3}>
-              <Row>
-                <Col lg={5}>
-                  <div className={styles.textualImg3}>
-                    <Image
-                      src={val.profile_pic}
-                      alt="card"
-                      width={150}
-                      height={150}
-                    />
-                  </div>
-                </Col>
-                <Col lg={7}>
-                  <div className={styles.textualContent3}>
-                    <h3>{val.title}</h3>
-                    <p>{val.position}</p>
-                    <h5>{val.subtitle}...</h5>
-
-                    {/* <Link className="border border-2 rounded p-2 " href={`/interview/textual-interviews/${val.id}`}> */}
-                    <Link className="border border-2 rounded p-2 " href={`#`}>
-                      Read More <MdKeyboardArrowRight />
-                    </Link>
-                  </div>
-                </Col>
-              </Row>
+    <div className={styles.heroWrapper}>
+      <Container>
+        <div className={styles.hero}>
+          <div className={styles.heroContent}>
+            <h1>Leaders Unfiltered: Share Your Story, Shape the Future.</h1>
+            <h1>Exclusive Interviews with Top Performers.</h1>
+            <p>
+              Get featured in exclusive interviews alongside industry
+              heavyweights. Exploring leadership across industries. Showcase
+              your expertise. Contact us today.
+            </p>
+          </div>
+          <div className={styles.heroImage}>
+            <div className={styles.imageContainer}>
+              <Image
+                className={styles.ellipseImg}
+                src={ellipseImg}
+                alt="ellipse"
+                width={400}
+                height={400}
+                layout="responsive"
+              />
             </div>
-          </Col>
-        ))}
-
-        {/* <Col lg={4}>
-          <div className={styles.textualCard3}>
-            <Row>
-              <Col lg={5}>
-                <div className={styles.textualImg3}>
-                  <Image src={cardImg5} alt="card" />
-                </div>
-              </Col>
-              <Col lg={7}>
-                <div className={styles.textualContent3}>
-                  <h3>Chris McDonald</h3>
-                  <p>Global Head of Technical Operations</p>
-                  <h5>
-                    Chris McDonald, Global Head of Technical Operations, on his
-                    Journey to Lead a Sustainable ...
-                  </h5>
-                  <MgButton>
-                    Read More <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
           </div>
-        </Col>
-        <Col lg={4}>
-          <div className={styles.textualCard3}>
-            <Row>
-              <Col lg={5}>
-                <div className={styles.textualImg3}>
-                  <Image src={cardImg6} alt="card" />
-                </div>
-              </Col>
-              <Col lg={7}>
-                <div className={styles.textualContent3}>
-                  <h3>Khang Pham Ngoc</h3>
-                  <p>Chief Financial Officer at Home Credit Vietnam</p>
-                  <h5>
-                    Home Credit Vietnam CFO Khang Pham Ngoc Tells us how he Came
-                    into his Role, and how…
-                  </h5>
-                  <MgButton>
-                    Read More <MdKeyboardArrowRight />
-                  </MgButton>
-                </div>
-              </Col>
-            </Row>
-          </div>
-        </Col> */}
-      </Row>
+        </div>
+      </Container>
     </div>
   );
 };
@@ -618,33 +397,7 @@ const ThirdRowCard = () => {
 const Interview = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.heroWrapper}>
-        <Container>
-          <div className={styles.hero}>
-            <div className={styles.heroContent}>
-              <h1>Leaders Unfiltered: Share Your Story, Shape the Future.</h1>
-              <h1>Exclusive Interviews with Top Performers.</h1>
-              <p>
-                Get featured in exclusive interviews alongside industry
-                heavyweights. Exploring leadership across industries. Showcase
-                your expertise. Contact us today.
-              </p>
-            </div>
-            <div className={styles.heroImage}>
-              <div className={styles.imageContainer}>
-                <Image
-                  className={styles.ellipseImg}
-                  src={ellipseImg}
-                  alt="ellipse"
-                  width={400}
-                  height={400}
-                  layout="responsive"
-                />
-              </div>
-            </div>
-          </div>
-        </Container>
-      </div>
+      <InterviewBanner />
       <div className={styles.videoTextualWrapper}>
         <Container>
           <VideoTextualInterview />
@@ -665,11 +418,8 @@ const Interview = () => {
       </div> */}
       <div className={styles.textualInterview}>
         <Container>
-          <div>
-            <FirstRowCard />
-            <SecondRowCard />
-            <ThirdRowCard />
-          </div>
+          <FirstRowCard />
+          <SecondRowCard />
         </Container>
       </div>
     </div>
